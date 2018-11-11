004041872e95be4754b1027f63c9e5ef457496c8
KateLynn Pullen <kdpullen95@gmail.com>
2018-11-11 10:07:39 -0500

    Update server.js to serve WebApp/index.html
    response from python process moved to console for now.

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ pythonProcName - change to ./SDSSQuery/whatever when you want to test
 var pythonProcName = "temp.py";
 console.log("starting...");
 
-var http = require('http');
 var url = require('url');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/', function (req, res) {
 
+  res.sendFile('WebApp/index.html', {root : __dirname});
 
   const spawn = require("child_process").spawn;
   const pythonProcess = spawn('python',[pythonProcName]); //[pythonProcName, argv1, argv2...]
   pythonProcess.stdout.on('data', (data) => {
     var txt = data.toString();
-    res.end(txt);
+    console.log("response from python process: ") //test
+    console.log(txt);
   });
 
-}).listen(8090);
+})
+
+app.listen(8090);
